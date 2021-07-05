ddfec66c582b2588b457f4eb06b93e197574f4c6
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2021-07-05 20:51:04 +0600

    no task exception

--- src/pages/Task/TaskList.tsx
+++ src/pages/Task/TaskList.tsx
@@ -98,7 +98,7 @@ const TaskList = ({ handleChangeTaskEditFormVisibility, lastAddedTask }: props) 
           ))}
         </div>
       ) : (
-        <div>{!!tasks && <p>No Tasks Found.</p>}</div>
+        <div className="p-4 rounded bg-gray-50 h-14">{!!tasks && <p>No Tasks Found.</p>}</div>
       )}
     </div>
   );
