7db0ad02b82f9324023a9ba807335703f3de343d
BanerjeeProdipta <64734379+BanerjeeProdipta@users.noreply.github.com>
2021-07-05 12:11:57 +0600

    delte api has cors issue

--- src/pages/Task/TaskList.tsx
+++ src/pages/Task/TaskList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { removeTask, taskToEdit, changeCompletionStatus } from '../../store/task/Actions';
+import { taskToEdit, changeCompletionStatus } from '../../store/task/Actions';
 import { ITask } from '../../types';
 import { BASE_URL } from '../../components/config';
 import axios from 'axios';
@@ -16,14 +16,24 @@ interface props {
 const TaskList = ({ handleChangeTaskEditFormVisibility, lastAddedTask }: props) => {
   const dispatch = useDispatch();
   const [tasks, setTasks] = useState<ITask[]>();
+  console.log(tasks);
 
   useEffect(() => {
     axios.get(`${BASE_URL}`).then((res) => setTasks(res.data));
   }, [lastAddedTask]);
 
-  const handleDeleteTask = (task: ITask) => {
-    if (task !== undefined) dispatch(removeTask(task));
-    toast.error('Task Deleted');
+  const handleDeleteTask = (taskID: string) => {
+    axios
+      .delete(`${BASE_URL}/${taskID}`, {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        toast.error('Task Deleted');
+      })
+      .catch((err) => console.log(err));
   };
 
   const handleEditTask = (task: ITask) => {
@@ -39,37 +49,37 @@ const TaskList = ({ handleChangeTaskEditFormVisibility, lastAddedTask }: props) 
       task.completionStatus = true;
       toast('Task Complete');
     }
-    dispatch(changeCompletionStatus(task));
-  };
 
-  const handleCheck = (task: ITask): boolean => {
-    return task.completionStatus === true ? true : false;
+    dispatch(changeCompletionStatus(task));
   };
 
   return (
     <div>
       {tasks && tasks.length > 0 && (
-        <div className="space-y-4 border rounded shadow-sm px-6 py-4 bg-white">
+        <div className="space-y-4 rounded shadow-sm px-6 py-4 bg-white">
           {tasks.map((task: ITask) => (
             <div
               key={task.id}
-              className={`p-4 border rounded ${task.completionStatus === true ? 'bg-gray-50' : 'hover:shadow-lg'}`}
+              className={`p-4 rounded ${task.completionStatus === true ? 'bg-gray-50' : 'hover:shadow-lg'}`}
             >
               <div className="cursor-pointer">
                 <div className="flex items-center">
-                  <input
-                    className="h-4 w-4 flex-shrink-0 mb-2"
-                    type="checkbox"
-                    id="completionStatus"
-                    checked={handleCheck(task)}
-                    onChange={() => handleChangeCompletionStatus(task)}
-                  ></input>
-                  <p className="text-2xl font-semibold text-indigo-700 mb-2 ml-2">{task.title}</p>
+                  <p className="text-2xl font-semibold text-indigo-700 mb-2">{task.title}</p>
                 </div>
                 <p>{task.body}</p>
               </div>
 
               <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 mt-4">
+                <button
+                  onClick={() => handleChangeCompletionStatus(task)}
+                  className={`w-full sm:w-auto bg-white font-bold leading-6 text-xs hover:text-white px-4 border-2 rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:outline-none transition-colors duration-200 ${
+                    task.completionStatus
+                      ? 'text-red-900 hover:bg-red-900 border-red-900 focus:ring-red-900'
+                      : 'text-green-900 hover:bg-green-900 border-green-900 focus:ring-green-900'
+                  }`}
+                >
+                  {task.completionStatus ? 'Incomplete' : 'Complete'}
+                </button>
                 <button
                   className="w-full sm:w-auto bg-white font-bold leading-6 text-blue-900 text-xs hover:bg-blue-900 hover:text-white px-4 border-blue-900 border-2 rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-900 focus:outline-none transition-colors duration-200"
                   onClick={() => handleEditTask(task)}
@@ -79,7 +89,7 @@ const TaskList = ({ handleChangeTaskEditFormVisibility, lastAddedTask }: props) 
 
                 <button
                   className="w-full sm:w-auto bg-white font-bold leading-6 text-pink-900 text-xs hover:bg-pink-900 hover:text-white px-4 border-pink-900 border-2 rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-pink-900 focus:outline-none transition-colors duration-200"
-                  onClick={() => handleDeleteTask(task)}
+                  onClick={() => handleDeleteTask(task.id)}
                 >
                   delete
                 </button>
